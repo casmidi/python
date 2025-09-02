f9aac8ffebe20ae4f1ab37064473b04d3fa219e3
casmidi <casmidiasli@yahoo.co.id>
2025-09-02 16:51:25 +0700

    latih14.py

---
+++ latih14.py
@@ -0,0 +1,125 @@
+#Variable String
+x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variabel integer
+x = int(20)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable float
+x = float(20.5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable complex
+x = complex(1j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable List
+x = list(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable tuple
+x = tuple(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable range
+x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable dict
+x = dict(name="John", age=36)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable set
+x = set(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable frozenset
+x = frozenset(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variabel bool
+x = bool(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Variable bytes
+x = bytes(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#Byte array
+x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#memoryview
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
