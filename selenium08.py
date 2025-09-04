2c51ebd08828831de7ef2b58da3f749be9d794aa
casmidi <casmidiasli@yahoo.co.id>
2025-09-04 16:36:35 +0700

    selenium08.py, login terlihat

---
+++ selenium08.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+chrome_options = Options()
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+driver = webdriver.Chrome(options=chrome_options)
+
+# Buka halaman login
+driver.get("https://www.saucedemo.com/")
+
+# Cari field username dan password
+username_field = driver.find_element(By.ID, "user-name")
+password_field = driver.find_element(By.ID, "password")
+
+# Isi form
+username_field.send_keys("standard_user")
+password_field.send_keys("secret_sauce")
+
+# Klik tombol login
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+
+# Tunggu sebentar supaya halaman sempat terbuka
+time.sleep(3)
+
+# Ambil judul halaman setelah login
+print("Title setelah login:", driver.title)
+
+driver.quit()
